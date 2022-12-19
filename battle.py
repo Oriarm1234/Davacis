951fdfaf54dda6ea9bdc5b74d0f6edb5a6bf8e67
Inofearu <inofearu@gmail.com>
2022-12-19 18:05:17 +0000

    battle.py progress on entity loading, goblin.json removal of type

--- battle.py
+++ battle.py
@@ -1,12 +1,10 @@
-from random import randint
-from random import choice
-from os import listdir
+import random
+import os
 import json
+from definitions import filePath
 class enemyClass:
     def __init__(self):
         self.name = ""
-        self.type = ""
-        self.race = {}
         self.health = 20
         self.level = 0
         self.dexterity = 1
@@ -30,28 +28,37 @@ class enemyClass:
         Awareness: {self.awareness}
         Charisma: {self.charisma}
         Intelligence: {self.intelligence}
-        Strength: {self.strength}"""
-    def chooseSelf(self,spawnList,filePath,secretCond = False,diffScale = False):
-        self.rarity = randint(1,101)
-        if diffScale != False:
-         self.rarity = self.rarity * diffScale
-        if self.rarity < 58: # 58%
-            self.rarity = "common"
-        elif self.rarity < 83: # 25%
-            self.rarity = "uncommon"
-        elif self.rarity < 93: # 10%
-            self.rarity = "epic"  
-        elif self.rarity < 98: # 5%
-            self.rarity = "legendary"
-        elif self.rarity < 100: # 2%
-            self.rarity = "mythical"
-        if secretCond != False:
-            if self.rarity == 101 and secretCond == True: # 1% and a condition must be true
-                self.rarity = "secret"
-        with open(f"{filePath}\\entities\\enemies\\{self.rarity}\\{choice(listdir(''))}") as f: # choice needs finishing
+        Strength: {self.strength}
+        Traits: {self.traits}
+        Proficiencies: {self.profs}
+        Drop List: {self.drops}"""
+    def chooseSelf(self,spawnList,filePath,secretCond = False,diffScale = False,forceEntity = False): # secretCond does not function in this implementation
+        self.rarity = random.randint(1,101)
+        if forceEntity == False:
+            if diffScale == True:
+                self.rarity = self.rarity * diffScale
+            if self.rarity < 58: # 58%
+                self.rarity = "common"
+            elif self.rarity < 83: # 25%
+                self.rarity = "uncommon"
+            elif self.rarity < 93: # 10%
+                self.rarity = "epic"  
+            elif self.rarity < 98: # 5%
+                self.rarity = "legendary"
+            elif self.rarity < 100: # 2%
+                self.rarity = "mythical"
+            if secretCond == True:
+                if self.rarity == 101 and secretCond == True: # 1% and a condition must be true
+                    self.rarity = "secret"
+        entityToLoad = f"{spawnList[self.rarity][random.randint(0,len(spawnList[self.rarity]))]}.json"
+        with open(f"{filePath}\\entities\\enemies\\{self.rarity}\\{entityToLoad}") as f: # choice needs finishing
             entityLoading = json.loads(f.read())
             for key in entityLoading:
                 setattr(self, key, entityLoading[key])
 enemy = enemyClass()
+spawnList = {
+    "common":["goblin","goblin"]
+}
+enemy.chooseSelf(spawnList,filePath)
 print(enemy)
     
